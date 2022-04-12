f0188a0308cfe657db2f0c38796d88371bae8c43
bc-ru <vladimir.boldyrev.2003@mail.ru>
2022-04-12 13:06:01 +0700

    bugfix

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 и некоторых доступных действий помощника.
 """
 import sys
+import threading
 
 from pynput import keyboard
 import dialog_window
@@ -14,8 +15,41 @@ import datetime
 from character_window import DialogWindow
 
 from PyQt5.QtWidgets import QMenu, QSystemTrayIcon, QAction, QWidget, QApplication
-from PyQt5.QtCore import pyqtSignal, QThread
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSignal, QThread, pyqtSlot
 from PyQt5.QtGui import QIcon
+
+
+def thread(my_func):
+    """
+    Запускает функцию в отдельном потоке
+    """
+
+    def wrapper(*args, **kwargs):
+        my_thread = threading.Thread(target=my_func, args=args, kwargs=kwargs)
+        my_thread.start()
+
+    return wrapper
+
+@thread
+def say(signal, text):
+    recor = sr.Recognizer()
+    micro = sr.Microphone(device_index=None)
+
+    with micro as source:
+        recor.adjust_for_ambient_noise(source, duration=1)
+        text.emit("- Слушаю...")
+        audio = recor.listen(source)
+    try:
+        zadanie = recor.recognize_google(audio, language="ru-RU").lower()
+
+    except sr.UnknownValueError:
+        zadanie = "- Я вас не поняла."
+
+    except sr.RequestError:
+        zadanie = "- К сожалению, я распознаю речь с интернетом("
+
+    signal.emit(zadanie)
 
 
 class Runner(QThread):
@@ -24,6 +58,7 @@ class Runner(QThread):
     change_text_enter = pyqtSignal(str)
     timer_exit = pyqtSignal(int)
     proc_comm = pyqtSignal(str)
+    voice = pyqtSignal(str)
 
     def __init__(self, config):
         super().__init__()
@@ -36,6 +71,7 @@ class Runner(QThread):
         self.voice_check = 0                                                       # Проверка на запуск голосового ввода
         self.quote = True
         self.config = config
+        self.voice.connect(self.say_command, QtCore.Qt.QueuedConnection)
         self.character = self.config["User"]["character"]
         self.image = "Image/Characters/%s_set/%s_main.png" % (self.character, self.character)
 
@@ -142,6 +178,8 @@ class Runner(QThread):
             self.ent_check = False
             self.window.enterWindow.hide()
 
+
+
     # Открытие и закрытие поля вывода помощника
     def close_ob(self):
         if self.window:
@@ -185,6 +223,10 @@ class Runner(QThread):
     def close(self):
         if self.window:
             self.flow.close()
+
+    def say_command(self, say_comm):
+        self.proc_comm.emit(say_comm)
+        self.voice_check = 0
 
     # Метод для получения времени для обращения к пользователю
     def change_window(self, window):
